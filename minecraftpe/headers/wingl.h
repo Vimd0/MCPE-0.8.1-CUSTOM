50fcdcc552149d604b27d9237cc7b025fa53761a
GameHerobrine <waterfall009@gmail.com>
2026-03-01 10:17:30 +0300

    imagine adding unused header

--- minecraftpe/headers/wingl.h
+++
@@ -1,9 +0,0 @@
-#ifndef WINGL_H
-#define WINGL_H
-#ifdef __WIN32__
-#include <gl/GL11.h>
-void (*glDeleteBuffers)(GLsizei n, const GLuint *buffers);
-void (*glBufferData)(GLenum target, GLsizeiptr size, const void *data, GLenum usage);
-void (*glBindBuffer)(GLenum target, GLuint buffer);
-#endif
-#endif
